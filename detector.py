f7fd82a5a8930fdf451049da4c2d943c6282a0fc
Luis <ldifernan@gmail.com>
2019-11-21 09:44:49 +0100

    Refactor

--- detector.py
+++ detector.py
@@ -3,7 +3,8 @@ from email.mime.multipart import MIMEMultipart
 import smtplib
 import logging
 
-logging.basicConfig(filename='detector.log', filemode='w', format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+## VARS
+
 pir = MotionSensor(4)
 led = LED(3)
 mail = "xxxx@xxx.com"
@@ -13,14 +14,18 @@ smtp_port = 587
 mail_from = "xxxx@xxx.com"
 mail_to = "yyyy@yyy.com"
 mail_subject = "Presence detected"
-led.off()
+log_format = '%(asctime)s - %(message)s'
+log_date_format = '%d-%b-%y %H:%M:%S'
+log_level = logging.INFO
+
+## FUNCTIONS
 
 
 def send_mail():
     s = smtplib.SMTP(host=smtp_host, port=smtp_port)
     s.starttls()
     s.login(mail, password)
-    logging.info("Login succesful")
+    logging.debug("Login succesful")
     msg = MIMEMultipart()
     msg['From'] = mail_from
     msg['To'] = mail_to
@@ -30,7 +35,12 @@ def send_mail():
     del msg
     s.quit()
 
+## MAIN
 
+
+logging.basicConfig(filename='detector.log', filemode='w', format=log_format,
+    datefmt=log_date_format, level=log_level)
+led.off()
 while True:
     pir.wait_for_motion()
     logging.info("Motion Detected")
