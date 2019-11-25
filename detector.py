77fd5b4d92a807b1d6d434512cdc12f8d8cdc38e
Luis <ldifernan@gmail.com>
2019-11-25 14:22:09 +0100

    Added several updates

--- detector.py
+++ detector.py
@@ -1,10 +1,20 @@
+from picamera import PiCamera
 from gpiozero import MotionSensor, LED
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 import smtplib
 import logging
+import time
+from datetime import datetime
 
 ## VARS
 
+
+now = datetime.now()
+timestr = now.strftime("%Y%m%d-%H%M%S")
+picture = "/home/pi/detector/" + timestr + ".jpg"
 pir = MotionSensor(4)
 led = LED(3)
 mail = "xxxx@xxx.com"
@@ -17,6 +27,7 @@ mail_subject = "Presence detected"
 log_format = '%(asctime)s - %(message)s'
 log_date_format = '%d-%b-%y %H:%M:%S'
 log_level = logging.INFO
+camera = PiCamera()
 
 ## FUNCTIONS
 
@@ -30,21 +41,56 @@ def send_mail():
     msg['From'] = mail_from
     msg['To'] = mail_to
     msg['Subject'] = mail_subject
+    # string to store the body of the mail
+    body = "Presence detected"
+    # attach the body with the msg instance
+    msg.attach(MIMEText(body, 'plain'))
+    # open the file to be sent
+    #filename = timestr + ".h264"
+    #attachment = open("/home/pi/detector/video" + filename, "rb")
+    filename = timestr + ".jpg"
+    attachment = open("/home/pi/detector/" + filename, "rb")
+    # instance of MIMEBase and named as p
+    p = MIMEBase('application', 'octet-stream')
+    # To change the payload into encoded form
+    p.set_payload((attachment).read())
+    # encode into base64
+    encoders.encode_base64(p)
+    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    # attach the instance 'p' to instance 'msg'
+    msg.attach(p)
     s.send_message(msg)
-    logging.info("Email sent to " + mail)
+    logging.info("Email sent to " + mail_to)
     del msg
     s.quit()
+
+
+def take_photo():
+    camera.start_preview()
+    time.sleep(3)
+    camera.capture('/home/pi/detector/' + timestr + '.jpg')
+    camera.stop_preview()
+
+
+def capture_video():
+    camera.start_preview()
+    camera.start_recording('/home/pi/detector/video' + timestr + '.h264')
+    time.sleep(5)
+    camera.stop_recording()
+    camera.stop_preview()
 
 ## MAIN
 
 
-logging.basicConfig(filename='detector.log', filemode='w', format=log_format,
-    datefmt=log_date_format, level=log_level)
-led.off()
+logging.basicConfig(filename='detector.log', filemode='a', format=log_format,
+                    datefmt=log_date_format, level=log_level)
+
 while True:
     pir.wait_for_motion()
     logging.info("Motion Detected")
-    send_mail()
     led.on()
+    take_photo()
+    send_mail()
     pir.wait_for_no_motion()
     led.off()
+    time.sleep(30)
